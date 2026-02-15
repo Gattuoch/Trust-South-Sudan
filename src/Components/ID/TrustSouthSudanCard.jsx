b7746f7f5a344eb566261d0e58690f7f3c9031f3
Gattuoch <gchambang@gmail.com>
2026-02-15 23:34:16 +0300

    camper Portal

--- src/Components/ID/TrustSouthSudanCard.jsx
+++ src/Components/ID/TrustSouthSudanCard.jsx
@@ -1,159 +1,133 @@
 import React from "react";
 import logo from "../../assets/Id-logo.png";
-import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
-import { BsBuilding } from "react-icons/bs";
+import { Phone, Mail, MapPin, Globe } from "lucide-react";
 
-export default function TrustCard() {
-  const icons = [FaPhoneAlt, FaEnvelope, BsBuilding, FaMapMarkerAlt];
-
+export default function TrustContactCard() {
   return (
-    <div className="bg-gray-200 flex justify-center py-6 px-3 print:bg-white">
-      {/* ================= POSTER ================= */}
-      <div
-        className="
-          relative
-          w-full
-          max-w-[720px]
-          bg-white
-          shadow-xl
-          border
-          text-[12px] sm:text-[13px]
-          leading-snug
-          font-sans
-          overflow-hidden
-          print:shadow-none
-          print:border-none
-        "
-      >
-        {/* ================= HEADER ================= */}
-        <div className="relative bg-white overflow-hidden">
-          {/* Top Curved Blue Shape */}
-          <div
-            className="
-              absolute top-0 left-0 w-full
-              h-[180px] sm:h-[220px] md:h-[240px]
-              bg-[#0e6f8f]
-              rounded-b-[120px] sm:rounded-b-[160px] md:rounded-b-[180px]
-            "
-          />
+    <div className="w-full max-w-[500px] 
+                    min-h-[650px] sm:min-h-[750px] 
+                    bg-[#e9e9e9] mx-auto relative 
+                    overflow-hidden shadow-2xl font-sans">
 
-          {/* Header Content */}
-          <div
-            className="
-              relative
-              flex flex-col sm:flex-row
-              items-center
-              justify-between
-              px-4 sm:px-8 md:px-10
-              pt-6 sm:pt-8
-              pb-6
-              gap-4 sm:gap-0
-            "
-          >
-            {/* Logo */}
-            <div className="bg-white rounded-full p-2 shadow-lg">
-              <img
-                src={logo}
-                alt="Trust Logo"
-                className="
-                  w-[90px] h-[90px]
-                  sm:w-[110px] sm:h-[110px]
-                  md:w-[120px] md:h-[120px]
-                  object-contain rounded-full
-                "
-              />
-            </div>
+      {/* ================= TOP CURVED HEADER ================= */}
+      <div className="relative bg-[#177a96] 
+                      h-[180px] sm:h-[220px] 
+                      rounded-b-[100%_100px] sm:rounded-b-[100%_120px] 
+                      flex items-center px-4 sm:px-6">
 
-            {/* Title */}
-            <div className="text-center sm:text-right leading-tight">
-              <h1
-                className="
-                  text-[38px] sm:text-[48px] md:text-[60px]
-                  font-black
-                  tracking-[2px] sm:tracking-[3px] md:tracking-[4px]
-                  text-black
-                  [text-stroke:1px_white]
-                  [-webkit-text-stroke:1px_white]
-                "
-              >
-                TRUST
-              </h1>
+        <img
+          src={logo}
+          alt="logo"
+          className="w-[80px] h-[80px] sm:w-[110px] sm:h-[110px] 
+                     rounded-full bg-white p-2"
+        />
 
-              <h2
-                className="
-                  text-[20px] sm:text-[26px] md:text-[34px]
-                  font-extrabold
-                "
-              >
-                <span className="text-red-600 underline decoration-2 underline-offset-4">
-                  SOUTH
-                </span>{" "}
-                <span className="text-green-600 underline">
-                  SUDAN
-                </span>
-              </h2>
-            </div>
-          </div>
+        <div className="ml-4 sm:ml-6">
+          <h1 className="text-[26px] sm:text-[40px] 
+                         font-extrabold text-black leading-none">
+            TRUST
+          </h1>
+          <h2 className="text-[20px] sm:text-[32px] 
+                         font-extrabold mt-1 sm:mt-2">
+            <span className="text-red-600">SOUTH </span>
+            <span className="text-green-500">SUDAN</span>
+          </h2>
         </div>
-
-        {/* ================= BODY SECTION ================= */}
-        <div className="relative mt-[40px] pb-[120px]">
-          
-          {/* ===== LEFT SIDEBAR ===== */}
-          <div className="absolute top-[0px] left-0 w-[70px] h-[350px] bg-[#0e6f8f] rounded-tr-[50px] rounded-br-[50px] flex flex-col items-center pt-[70px] gap-[38px] z-20">
-            {icons.map((Icon, index) => (
-              <div
-                key={index}
-                className="w-[40px] h-[40px] bg-black rounded-full text-white flex items-center justify-center text-[18px]"
-              >
-                <Icon />
-              </div>
-            ))}
-          </div>
-
-          {/* ===== CONTENT ===== */}
-          <div className="ml-[100px] mr-[20px] text-[16px]">
-            <div className="mb-[42px]">
-              <p>Zain +211911743465</p>
-              <p>MTN +211927863377</p>
-            </div>
-
-            <div className="mb-[42px]">
-              <p>trustsouthsudan@gmail.com</p>
-            </div>
-
-            <div className="mb-[42px]">
-              <p>https://trust-south-sudan.vercel.app</p>
-            </div>
-
-            <div>
-              <p>Juba South Sudan</p>
-              <p>Gudele West block two</p>
-            </div>
-          </div>
-
-          {/* ===== GREEN TOP LEFT ===== */}
-          <div className="absolute top-[-10px] left-[-60px] w-[140px] h-[140px] bg-[#59b548] rounded-[100px] z-10"></div>
-
-          {/* ===== GREEN BOTTOM LEFT ===== */}
-          <div className="absolute bottom-[40px] left-[-60px] w-[150px] h-[90px] bg-[#59b548] rounded-[100px] z-10"></div>
-
-          {/* ===== GREEN DIAGONAL ===== */}
-          <div className="
-      absolute
-      bottom-[-50px] sm:bottom-[-60px]
-      left-[-10%] sm:left-[-2%]
-      w-[140%] sm:w-[120%]
-      h-[100px] sm:h-[120px]
-      bg-[#59b548] 
-      rotate-[-6deg] sm:rotate-[-7deg]
-    "
-  ></div>
-        </div>
-
-        {/* ===== BOTTOM BLACK LINE ===== */}
-        <div className="absolute bottom-0 left-0 w-full h-[20px] bg-black"></div>
       </div>
+
+      {/* ================= LEFT SIDEBAR DESIGN ================= */}
+      <div className="absolute top-[180px] sm:top-[220px] 
+                      left-0 h-[420px] sm:h-[480px] 
+                      w-[60px] sm:w-[85px] 
+                      bg-[#156f8f] overflow-hidden">
+
+        {/* Top Green Curve */}
+        <div className="absolute -top-[45px] -left-[30px] 
+                        w-[130px] h-[130px] sm:w-[170px] sm:h-[170px] 
+                        bg-[#5bb949] rounded-full">
+        </div>
+
+        {/* Bottom Green Curve */}
+        <div className="absolute -bottom-[45px] -left-[30px] 
+                        w-[130px] h-[130px] sm:w-[170px] sm:h-[170px] 
+                        bg-[#5bb949] rounded-full">
+        </div>
+      </div>
+
+      {/* ================= CONTACT SECTION ================= */}
+      <div className="relative 
+                      pt-8 sm:pt-12 
+                      pl-[15px] sm:pl-[25px] 
+                      pr-4 sm:pr-6 
+                      mt-[10px] sm:mt-[20px] 
+                      space-y-6 sm:space-y-10 
+                      pb-[160px] sm:pb-[200px]">
+
+        {/* Phone */}
+        <div className="flex items-start">
+          <div className="w-[60px] sm:w-[85px] flex justify-center">
+            <div className="bg-black p-2 sm:p-3 rounded-full text-white">
+              <Phone size={18} className="sm:w-[22px] sm:h-[22px]" />
+            </div>
+          </div>
+          <div className="text-[16px] sm:text-[20px] font-medium leading-6 sm:leading-8">
+            <p>Zain +211911743465</p>
+            <p>MTN +211927863377</p>
+          </div>
+        </div>
+
+        {/* Email */}
+        <div className="flex items-start">
+          <div className="w-[60px] sm:w-[85px] flex justify-center">
+            <div className="bg-black p-2 sm:p-3 rounded-full text-white">
+              <Mail size={18} className="sm:w-[22px] sm:h-[22px]" />
+            </div>
+          </div>
+          <p className="text-[16px] sm:text-[20px] font-medium break-all">
+            trustsouthsudan@gmail.com
+          </p>
+        </div>
+
+        {/* Website */}
+        <div className="flex items-start">
+          <div className="w-[60px] sm:w-[85px] flex justify-center">
+            <div className="bg-black p-2 sm:p-3 rounded-full text-white">
+              <Globe size={18} className="sm:w-[22px] sm:h-[22px]" />
+            </div>
+          </div>
+          <p className="text-[16px] sm:text-[20px] font-medium break-all">
+            https://trust-south-sudan.vercel.app
+          </p>
+        </div>
+
+        {/* Address */}
+        <div className="flex items-start">
+          <div className="w-[60px] sm:w-[85px] flex justify-center">
+            <div className="bg-black p-2 sm:p-3 rounded-full text-white">
+              <MapPin size={18} className="sm:w-[22px] sm:h-[22px]" />
+            </div>
+          </div>
+          <div className="text-[16px] sm:text-[20px] font-medium leading-6 sm:leading-8">
+            <p>Juba South Sudan</p>
+            <p>Gudele West block two</p>
+          </div>
+        </div>
+
+      </div>
+
+      {/* ================= BOTTOM GREEN ANGLED SHAPE ================= */}
+      <div className="absolute bottom-0 left-0 
+                      w-full h-[110px] sm:h-[140px] 
+                      bg-[#57b847] 
+                      [clip-path:polygon(0_60%,100%_40%,100%_100%,0%_100%)]">
+      </div>
+
+      {/* ================= BLACK STRIP ================= */}
+      <div className="absolute bottom-0 left-0 
+                      w-full h-[25px] sm:h-[35px] 
+                      bg-black">
+      </div>
+
     </div>
   );
 }
